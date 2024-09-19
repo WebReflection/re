56cbbf2904afb85b936a5e7fbfab065ac9eaf64d
webreflection <andrea.giammarchi@gmail.com>
2024-09-19 09:45:03 +0200

    better naming convention

--- esm/index.js
+++ esm/index.js
@@ -1,7 +1,7 @@
 import escapeString from 'escape-string-regexp';
 
-const escape = RegExp.escape || escapeString;
-const scape = value => escape(value);
+const $ = RegExp.escape || escapeString;
+const escape = value => $(value);
 
 /**
  * Sanitize interpolations for safer RegExp definition.
@@ -10,9 +10,9 @@ const scape = value => escape(value);
  * @returns {RegExp}
  */
 export default ({ raw }, ...values) => {
-  let [e] = raw, { length } = raw, i = 0;
+  let [s] = raw, { length } = raw, i = 0;
   while (++i < length)
-    e += [].concat(values[i - 1]).map(scape).join('|') + raw[i];
-  i = e.lastIndexOf('/');
-  return new RegExp(e.slice(1, i), e.slice(i + 1));
+    s += [].concat(values[i - 1]).map(escape).join('|') + raw[i];
+  i = s.lastIndexOf('/');
+  return new RegExp(s.slice(1, i), s.slice(i + 1));
 };
